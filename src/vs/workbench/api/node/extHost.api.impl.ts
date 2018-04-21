1622e966135f4fe180bdf9240234720e80997be5
Andre Weinand <aweinand@microsoft.com>
2018-04-21 02:45:43 +0200

    do not throw on proposed API usage

--- src/vs/workbench/api/node/extHost.api.impl.ts
+++ src/vs/workbench/api/node/extHost.api.impl.ts
@@ -72,8 +72,8 @@ export function checkProposedApiEnabled(extension: IExtensionDescription): void 
 	}
 }
 
-function throwProposedApiError(extension: IExtensionDescription): never {
-	throw new Error(`[${extension.id}]: Proposed API is only available when running out of dev or with the following command line switch: --enable-proposed-api ${extension.id}`);
+function throwProposedApiError(extension: IExtensionDescription): void {
+	// throw new Error(`[${extension.id}]: Proposed API is only available when running out of dev or with the following command line switch: --enable-proposed-api ${extension.id}`);
 }
 
 function proposedApiFunction<T>(extension: IExtensionDescription, fn: T): T {
