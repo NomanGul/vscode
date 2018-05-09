25ae9712b9bd15ff9940027482b7e48a82f3ee3d
Martin Aeschlimann <martinae@microsoft.com>
2018-05-09 17:14:49 +0200

    make sure builtInExtensions are fetched

--- scripts/remoteExtensionAgent.sh
+++ scripts/remoteExtensionAgent.sh
@@ -10,6 +10,9 @@ fi
 function code() {
 	cd $ROOT
 
+	# Sync built-in extensions
+	node build/lib/builtInExtensions.js
+
 	NODE_ENV=development \
 	VSCODE_DEV=1 \
 	node "$ROOT/out/remoteExtensionHostAgent.js" --builtin-extensions=extension-editing,configuration-editing,search-rg,css-language-features,git,grunt,gulp,html-language-features,json-language-features,markdown-language-features,npm,php-language-features,typescript-language-features,../.build/builtInExtensions/ms-vscode.node-debug,../.build/builtInExtensions/ms-vscode.node-debug2 "$@"
