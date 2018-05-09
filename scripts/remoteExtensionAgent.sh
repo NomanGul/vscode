54f491aa1c1736869d061bcb0c4b3d69c5881eb9
Martin Aeschlimann <martinae@microsoft.com>
2018-05-09 17:14:31 +0200

    Workaround for #33 to fix code completion in setting editors

--- scripts/remoteExtensionAgent.sh
+++ scripts/remoteExtensionAgent.sh
@@ -12,7 +12,7 @@ function code() {
 
 	NODE_ENV=development \
 	VSCODE_DEV=1 \
-	node "$ROOT/out/remoteExtensionHostAgent.js" --builtin-extensions=search-rg,css-language-features,git,grunt,gulp,html-language-features,json-language-features,markdown-language-features,npm,php-language-features,typescript-language-features,../.build/builtInExtensions/ms-vscode.node-debug,../.build/builtInExtensions/ms-vscode.node-debug2 "$@"
+	node "$ROOT/out/remoteExtensionHostAgent.js" --builtin-extensions=extension-editing,configuration-editing,search-rg,css-language-features,git,grunt,gulp,html-language-features,json-language-features,markdown-language-features,npm,php-language-features,typescript-language-features,../.build/builtInExtensions/ms-vscode.node-debug,../.build/builtInExtensions/ms-vscode.node-debug2 "$@"
 }
 
 code "$@"
