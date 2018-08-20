d3823e1b3e598d75a5c47c8d0e0d41ac1e6a8b40
Daniel Imms <daimms@microsoft.com>
2018-08-20 15:02:25 -0700

    Fix terminal default shell workaround

--- src/vs/workbench/parts/terminal/node/terminal.ts
+++ src/vs/workbench/parts/terminal/node/terminal.ts
@@ -24,7 +24,7 @@ export interface ITerminalChildProcess {
 	resize(cols: number, rows: number): void;
 }
 
-let _TERMINAL_DEFAULT_SHELL_UNIX_LIKE: string = '/bin/bash';
+let _TERMINAL_DEFAULT_SHELL_UNIX_LIKE: string | null = null;
 export function getTerminalDefaultShellUnixLike(): string {
 	if (!_TERMINAL_DEFAULT_SHELL_UNIX_LIKE) {
 		let unixLikeTerminal = 'sh';
