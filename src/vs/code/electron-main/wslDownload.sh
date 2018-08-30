96e23979278aefd74227c9003af0baefc407098f
Alex Dima <alexdima@microsoft.com>
2018-08-30 17:29:28 +0200

    Better progress for wslDownload.sh

--- src/vs/code/electron-main/wslDownload.sh
+++ src/vs/code/electron-main/wslDownload.sh
@@ -11,6 +11,17 @@ VSCODE_REMOTE_BIN="$HOME/.vscode-remote/bin"
 DOWNLOAD_URL="https://az764295.vo.msecnd.net/wsl/$COMMIT/vscode-headless-linux-x64.tar.gz"
 CLI_JS_PATH="$(dirname "$(realpath "$0")")/../../../cli-wsl.js"
 
+download()
+{
+	local url=$1
+	local name=$2
+	echo -n "    "
+	wget -O $name --progress=dot $url 2>&1 | grep --line-buffered "%" | \
+		sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}'
+	echo -ne "\b\b\b\b"
+	echo " DONE"
+}
+
 # Check if this version is already installed
 if [ ! -d "$VSCODE_REMOTE_BIN/$COMMIT" ]; then
 	# This version does not exist
@@ -25,14 +36,14 @@ if [ ! -d "$VSCODE_REMOTE_BIN/$COMMIT" ]; then
 	# Download the .tar.gz file
 	TMP_NAME="$COMMIT-$(date +%s)"
 	echo "Downloading...";
-	wget -O "$VSCODE_REMOTE_BIN/$TMP_NAME.tar.gz" $DOWNLOAD_URL
+	download $DOWNLOAD_URL "$VSCODE_REMOTE_BIN/$TMP_NAME.tar.gz"
 
 	# Unpack the .tar.gz file to a temporary folder name
 	echo "Unpacking...";
 	mkdir "$VSCODE_REMOTE_BIN/$TMP_NAME"
-	tar -xf "$VSCODE_REMOTE_BIN/$TMP_NAME.tar.gz" -C "$VSCODE_REMOTE_BIN/$TMP_NAME" --strip-components 1
+	tar -xf "$VSCODE_REMOTE_BIN/$TMP_NAME.tar.gz" -C "$VSCODE_REMOTE_BIN/$TMP_NAME" --strip-components 1 --verbose
 
-	echo "Copying CLI...";
+	echo "Copying scripts...";
 	cp "$CLI_JS_PATH" "$VSCODE_REMOTE_BIN/$TMP_NAME"
 
 	# Rename temporary folder to final folder name
