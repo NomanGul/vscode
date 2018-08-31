b86f38ba99b7f4352c89cd5b2bef6b5946147d69
Martin Aeschlimann <martinae@microsoft.com>
2018-08-31 20:53:09 +0200

    fix code-wsl.sh (debug argument problem)

--- resources/win32/bin/code.sh
+++ resources/win32/bin/code.sh
@@ -11,7 +11,11 @@ if grep -q Microsoft /proc/version; then
 	VSCODE_REMOTE_BIN="$HOME/.vscode-remote/bin"
 	"$VSCODE_PATH/resources/app/out/vs/code/electron-main/wslDownload.sh" $COMMIT
 	if [ "$1" == "--headless" ]; then
-		"$VSCODE_REMOTE_BIN/$COMMIT/node" "$2" "$VSCODE_REMOTE_BIN/$COMMIT/out/remoteExtensionHostAgent.js"
+		if [[ $2 ]]; then
+			"$VSCODE_REMOTE_BIN/$COMMIT/node" "$2" "$VSCODE_REMOTE_BIN/$COMMIT/out/remoteExtensionHostAgent.js"
+		else
+			"$VSCODE_REMOTE_BIN/$COMMIT/node" "$VSCODE_REMOTE_BIN/$COMMIT/out/remoteExtensionHostAgent.js"
+		fi
 	else
 		VSCODE_WIN_CMD="$SCRIPT_NAME.cmd"
 		"$VSCODE_REMOTE_BIN/$COMMIT/node" "$VSCODE_REMOTE_BIN/$COMMIT/cli-wsl.js" "$SCRIPT_NAME" "$VSCODE_WIN_CMD" $@
